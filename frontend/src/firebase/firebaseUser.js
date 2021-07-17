c7a20721115c800bcf00dc8e6a7d58af2430eb50
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-07-16 19:29:20 -0700

    firebase user functions

--- frontend/src/firebase/firebaseUser.js
+++ frontend/src/firebase/firebaseUser.js
@@ -1,9 +1,71 @@
-import { auth, firestore } from "./firebaseConfig";
+import { userReAuth } from "./firebaseAuth";
+import { auth } from "./firebaseConfig";
+
+const user = auth.currentUser
 
 // Update Username
-
+export const updateUserName = async (newName) => {
+  try {
+    await user.updateProfile({
+      displayName: newName
+    })
+    return 'Updated User Name Successfully'
+  } catch (error) {
+    return error
+  }
+}
 
 // Update Email
-
+export const updateEmail = async (newEmail) => {
+  try {
+    await user.updateEmail(newEmail)
+    return 'Email Updated Successfully'
+  } catch (error) {
+    return error
+  }
+}
 
 // Update Password
+export const updateEmail = async (currentPassword, newPassword) => {
+  try {
+    await userReAuth(currentPassword)
+    await user.updatePassword(newPassword)
+  } catch (error) {
+    return error
+  }
+}
+
+//TODO: Test this and remove unnecessary functions
+// Update User // All Previous functions combined into one function
+export const updateUser = async (newName, newEmail, newPassword, currentPassword) => {
+  const user = auth.currentUser
+
+  if (newName) {
+    try {
+      await user.updateProfile({
+        displayName: newName
+      })
+      return 'Updated User Name Successfully'
+    } catch (error) {
+      return error
+    }
+  }
+
+  if (newEmail) {
+    try {
+      await user.updateEmail(newEmail)
+      return 'Email Updated Successfully'
+    } catch (error) {
+      return (error)
+    }
+  }
+
+  if (newPassword) {
+    try {
+      await userReAuth(currentPassword)
+      await user.updatePassword(newPassword)
+    } catch (error) {
+      return error
+    }
+  }
+}
