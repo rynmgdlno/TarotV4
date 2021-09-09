66038720a3d6405c4ae690df81c92cec053f5d89
rynmgdlno <rynmgdlno@gmail.com>
2021-09-09 13:48:31 -0700

    new branch

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -8,7 +8,6 @@ require('dotenv').config({
 
 console.log(process.env.REACT_APP_API_KEY)
 
-
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
