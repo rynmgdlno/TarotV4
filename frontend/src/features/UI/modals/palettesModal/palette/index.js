47e9cf6c248975c8d67e23558f5b4aeb78b5986a
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-09-16 17:27:15 -0700

    individual pallete context menu styles done

--- frontend/src/features/UI/modals/palettesModal/palette/index.js
+++ frontend/src/features/UI/modals/palettesModal/palette/index.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { changeColor } from '../../../composer/color/editor/slider/channelEditorSlice'
+import { darkModeSelector } from '../../../darkMode/darkModeSlice'
 import { menuSelector, menuToggle } from '../../../menu/menuSlider/menuSliderSlice'
 import { palettesToggle } from '../palettesSlice'
 
 import CustomButton from '../../../../../components/custom-button'
-
-import './palette.scss'
+import Dots from '../../../../../components/SVG/dots'
 
 // Individual color component
 const Color = (values) => {
-  const {red, green, blue} = values.values
+  const { red, green, blue } = values.values
   const bgColor = { backgroundColor: `rgb(${red}, ${green}, ${blue})` }
-  return <div className='ind-color' style={bgColor}/>
+  return <div className='ind-color' style={bgColor} />
 }
 
-const Palette = ({ key, data }) => {
-  const dispatch = useDispatch()
-  const {name, palette} = data
+const fill = `rgba(119, 119, 119, .5)`
 
+const Palette = ({ key, data }) => {
+  const [showButtons, setShowButtons] = useState(false)
+  const buttonClass = showButtons ? '' : 'buttons-hidden'
+  const dispatch = useDispatch()
+  const darkMode = useSelector(darkModeSelector)
+  const fill = darkMode ? '#FAFAFA' : '#212121'
+  const { name, palette } = data
+  console.log(buttonClass)
   const loadPalette = (palette) => {
     dispatch(changeColor(palette))
     dispatch(menuToggle())
@@ -28,18 +34,28 @@ const Palette = ({ key, data }) => {
 
   return (
     <div key={key} className='palette'>
-      <p>{name}</p>
+      <span className='palette-top'>
+        <p>{name}</p>
+        <CustomButton
+          className='dots'
+          onClick={() => setShowButtons(!showButtons)}>
+          <Dots fill={fill} />
+        </CustomButton>
+      </span>
       <span className='colors'>
         {
           palette.map((color, i) => (
-            <Color values={color}/>
+            <Color values={color} />
           ))
         }
-      </span>
-      <span>
-        <CustomButton onClick={() => loadPalette(palette)}>Open</CustomButton>
-        <CustomButton>Rename</CustomButton>
-        <CustomButton>Delete</CustomButton>
+        <div className={`${buttonClass} palette-buttons`}>
+          <CustomButton className='palette-button' onClick={() => {
+            loadPalette(palette)
+            setShowButtons(false)
+          }}>Open</CustomButton>
+          <CustomButton className='palette-button'>Rename</CustomButton>
+          <CustomButton className='palette-button'>Delete</CustomButton>
+        </div>
       </span>
     </div>
   )
