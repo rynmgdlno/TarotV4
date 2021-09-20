ce362275589b6fa0402b45b26183dd9926b78715
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-09-19 20:04:11 -0700

    Add neccessary reducers and state for fetch

--- frontend/src/features/DATA/DATAReducer.js
+++ frontend/src/features/DATA/DATAReducer.js
@@ -3,16 +3,40 @@ import { createSlice } from "@reduxjs/toolkit";
 export const DATASlice = createSlice({
   name: 'DATASlice',
   initialState: {
+    activeQueryResult: 0,
+    currentPage: null,
     currentUser: null,
-    savedPalettes: null
+    isLoading: false,
+    savedPalettes: null,
+    swipeDelta: false,
+    queryPages: null,
+    queryResult: []
   },
   reducers: {
+    setActiveQueryResult: (state, action) => {
+      state.activeQueryResult = action.payload
+    },
+    setCurrentPage: (state, action) => {
+      state.currentPage = action.payload
+    },
     setCurrentUser: (state, action) => {
       state.currentUser = action.payload
     },
+    setIsLoading: (state) => {
+      state.isLoading = !state.isLoading
+    },
     setSavedPalettes: (state, action) => {
       state.savedPalettes = action.payload
-    }
+    },
+    setSwipeDelta: (state, action) => {
+      state.swipeDelta = action.payload
+    },
+    setQueryPages: (state, action) => {
+      state.queryPages = action.payload
+    },
+    setQueryResult: (state, action) => {
+      state.queryResult = action.payload
+    },
   }
 })
 
@@ -20,5 +44,11 @@ export const DATASlice = createSlice({
 export const currentUserSelector = state => state.data.currentUser
 export const savedPalettesSelector = state => state.data.savedPalettes
 
-export const { setCurrentUser, setSavedPalettes } = DATASlice.actions
+export const {
+  setCurrentPage,
+  setCurrentUser,
+  setIsLoading,
+  setSavedPalettes,
+  setQueryResult } = DATASlice.actions
+
 export default DATASlice.reducer
